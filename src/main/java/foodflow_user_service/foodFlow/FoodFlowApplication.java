1bcfc01417bc6f9c6ca6b02e45fc806411c04c1f
AditiDhoni <aditidhoni231@gmail.com>
2026-09-20 16:04:34 +0530

    Initial commit

---
+++ src/main/java/foodflow_user_service/foodFlow/FoodFlowApplication.java
@@ -0,0 +1,13 @@
+package foodflow_user_service.foodFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foodflow_user_service/foodFlow/FoodFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package foodflow_user_service.foodFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
